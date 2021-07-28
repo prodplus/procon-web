fe5a8fb645883ef291218ee3fbc1d90ed4ec6ad5
Marlon F. Garcia <marlon_garcia@outlook.com>
2021-07-28 13:53:02 -0300

    first commit

---
+++ proconweb-api/src/main/java/br/com/procon/ProconwebApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.procon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProconwebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProconwebApiApplication.class, args);
+	}
+
+}
---
+++ proconweb-api/src/main/java/br/com/procon/models/Perfil.java
@@ -0,0 +1,62 @@
+package br.com.procon.models;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * 
+ * @author Marlon Fernando Garcia
+ *
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Perfil implements Serializable {
+
+	private static final long serialVersionUID = 6614663084292025595L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	@Column(nullable = false, unique = true, length = 20)
+	private String role;
+	@Column(nullable = false, length = 50)
+	private String descricao;
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((role == null) ? 0 : role.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Perfil other = (Perfil) obj;
+		if (role == null) {
+			if (other.role != null)
+				return false;
+		} else if (!role.equals(other.role))
+			return false;
+		return true;
+	}
+
+}
